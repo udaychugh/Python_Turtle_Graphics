7fc6609a80653c307299863e3e91d0382025e938
udaychugh <udaychugh8@gmail.com>
2021-06-12 11:19:43 +0530

    update upto 12.06.2021

---
+++ hungama design/main.py
@@ -0,0 +1,14 @@
+import turtle
+
+Colors = ["orange","red","pink", "yellow","blue","green"]
+
+screen=turtle.Screen()
+trtl=turtle.Turtle()
+trtl.speed(0)
+screen.bgcolor('black')
+
+for x in range (360):
+    trtl.pencolor(Colors[x%6])
+    trtl.width(x/5+1)
+    trtl.forward(x)
+    trtl.left(20)
